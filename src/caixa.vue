fa55867fdfdcc495c3f5de8cbbe4510bbae5ffee
Maria <mariacarolina789@hotmail.comgit config --global user.name>
2023-10-09 21:41:20 -0300

    .

---
+++ src/caixa.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="cxa" v-if="contador<6">
+        <div>
+            caixa com nome {{ nome }}
+        </div>
+        <div>
+            contador {{ contador }}
+            <button @click="incrementa">
+                Incrementar 
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            contador: 1,
+            nome: "Maria"
+        }
+    },
+    methods: {
+        incrementa() {
+            this.contador++;
+        }
+
+    }
+}
+
+</script>
+
+<style > .cxa {
+     border: 1px solid black;
+     padding: 2px;
+     margin: 2px;
+ }
+</style>
